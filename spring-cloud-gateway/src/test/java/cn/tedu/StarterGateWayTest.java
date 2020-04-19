fbbdcafebf786d5a06f2a14e0cf9a4807daab77b
lijie <jd_lijie@163.com>
2020-04-19 10:42:34 +0800

    4

---
+++ spring-cloud-gateway/src/test/java/cn/tedu/StarterGateWayTest.java
@@ -0,0 +1,20 @@
+package cn.tedu;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple StarterGateWay.
+ */
+public class StarterGateWayTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
